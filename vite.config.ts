38af8cad1006e502a8a65a82f3023f77bcf6b02f
asd4a10 <rustem.amanzholov@nu.edu.kz>
2023-11-21 22:03:09 +0000

    auto deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://github.com/asd4a10/intern-io/tree/gh-pages",
+  base: "https://asd4a10.github.io/intern-io",
 });
