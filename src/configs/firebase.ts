eccce1739bc8a0ee91dcc6308952eda5c87be40d
asd4a10 <rustem.amanzholov@nu.edu.kz>
2023-12-07 23:27:33 +0000

    change analytics key

--- src/configs/firebase.ts
+++ src/configs/firebase.ts
@@ -17,7 +17,7 @@ const firebaseConfig = {
   storageBucket: "intern-io-4a22b.appspot.com",
   messagingSenderId: "673700748809",
   appId: "1:673700748809:web:1061dac4384b951932e2d8",
-  measurementId: "G-GM1GQPWP2L",
+  measurementId: "G-CW4T0PR53E",
   databaseURL:
     "https://intern-io-4a22b-default-rtdb.europe-west1.firebasedatabase.app",
 };
